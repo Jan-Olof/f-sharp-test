e6233e9400ec2ca6faf0be5efd387de96840b38a
Jan-Olof Nilsson <janolof@gmail.com>
2019-07-09 14:39:57 +0200

    Add project files.

---
+++ f-sharp-test/Scratchpad.fsx
@@ -0,0 +1,21 @@
+﻿let text = "Hello, world"
+text.Length
+
+let greetPerson name age =
+    sprintf "Hello, %s. You are %d years old" name age
+
+let greeting = greetPerson "Fred" 25
+
+let int2String (x: int) = string x
+
+open System.IO // Name spaces can be opened just as modules
+let countWords (text:string) =
+    let x = text.Split ' '
+    File.WriteAllText(@"d:\test.txt", "Text: " + text + ". Words: " +  int2String x.Length)
+    x.Length
+
+let cw = countWords "kalle och musse"
+
+File.WriteAllText(@"d:\test.txt", "Hello There\n Welcome to:\n Tutorials Point")
+let msg = File.ReadAllText(@"d:\test.txt")
+printfn "%s" msg
---
+++ f-sharp-test/Script1.fsx
@@ -0,0 +1,7 @@
+﻿let x = "hej hå!"
+
+let r = System.Random 1
+let g() = r.Next 10
+let h(inn) = r.Next inn
+
+let i = h 10
