9a54401886bfa64d29adb9754b59bf2a4f1a7717
Jan-Olof Nilsson <janolof@gmail.com>
2019-07-10 16:23:10 +0200

    4-WebBrowserForm

---
+++ f-sharp-test/4-WebBrowserForm.fsx
@@ -0,0 +1,16 @@
+﻿open System
+open System.Net
+open System.Windows.Forms
+
+let browserFunc(uri, text) =
+    let fsharpOrg =
+        let webClient = new WebClient()
+        webClient.DownloadString(Uri uri)
+
+    let form = new Form(Text = text)
+    let browser = new WebBrowser(ScriptErrorsSuppressed = true, Dock = DockStyle.Fill, DocumentText = fsharpOrg)
+
+    form.Controls.Add browser
+    form.Show()
+
+browserFunc("http://fsharp.org", "Hello from F#!")
