012482a2f1adb315c25dadd1d8015505663123f5
Jan-Olof Nilsson <janolof@gmail.com>
2019-07-12 18:08:53 +0200

    5

--- f-sharp-test/scripts/5-compiler.fsx
+++ f-sharp-test/scripts/5-compiler.fsx
@@ -7,3 +7,17 @@ let add2 (a, b) =
     answer
 
 let x = add2("då", "re")
+
+let sayHello(someValue) =
+    let innerFunction(number) =
+        if number > 10 then "Isaac"
+        elif number > 20 then "Fred"
+        else "Sara"
+
+    let resultOfInner =
+        if someValue < 10.0 then innerFunction(5)
+        else innerFunction(15)
+
+    "Hello " + resultOfInner
+
+let result = sayHello(10.5)
