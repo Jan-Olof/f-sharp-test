1c2d87de34df1f279ce174f22954103c0abe3e50
Jan-Olof Nilsson <janolof@gmail.com>
2019-07-15 11:03:16 +0200

    7

---
+++ f-sharp-test/scripts/7.fsx
@@ -0,0 +1,15 @@
+﻿open System
+let describeAge(age) =
+    let ageDescription =
+        if age < 18 then "Child"
+        elif age < 65 then "Adult"
+        else "OAP"
+
+    let greeting = "Hello"
+    Console.WriteLine("{0}! You are a '{1}'", greeting, ageDescription)
+
+let x = describeAge 20
+
+let u = ()
+
+if x = u then Console.WriteLine("equal") else Console.WriteLine("not")
