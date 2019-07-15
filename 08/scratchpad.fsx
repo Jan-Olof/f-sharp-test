322d4659912b541f7e294590cee25d3553120331
Jan-Olof Nilsson <janolof@gmail.com>
2019-07-15 14:51:12 +0200

    8b

--- 08/scratchpad.fsx
+++ 08/scratchpad.fsx
@@ -1,12 +1,19 @@
 open System
 
-/// Gets the distance to a given destination 
+/// Gets the distance to a given destination
 let getDistance (destination) =
     if destination = "Gas" then 10
-    // fill in the blanks!
+    elif destination = "Home" then 25
+    elif destination = "Office" then 50
+    elif destination = "Stadium" then 25
     else failwith "Unknown destination!"
+
+let calcRemainingPetrol(current, distance) =
+    if current >= distance then current-distance
+    else failwith "Oops! Tou've run out of petrol!"
 
 // Couple of quick tests
 getDistance("Home") = 25
-getDistance("Stadium") = 25
+getDistance("Office") = 50
 
+calcRemainingPetrol(100, 125)
