8c5d9428fde86b576ccf7d75e0792238e6f23a77
Jan-Olof Nilsson <janolof@gmail.com>
2019-07-17 19:19:56 +0200

    10a

---
+++ f-sharp-test/scripts/10-records.fsx
@@ -0,0 +1,9 @@
+﻿type Car =
+    { Manufacturer : string
+      EngineSize : string
+      NumberOfDoors : int }
+
+let car =
+    { Manufacturer = "Skoda"
+      EngineSize = "small"
+      NumberOfDoors = 4}
