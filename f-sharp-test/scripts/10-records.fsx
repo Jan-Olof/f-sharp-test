68b4a5e4ee7d9707e6a6902c497e86074e0bd770
Jan-Olof Nilsson <janolof@gmail.com>
2019-07-18 16:07:30 +0200

    10b

--- f-sharp-test/scripts/10-records.fsx
+++ f-sharp-test/scripts/10-records.fsx
@@ -3,7 +3,30 @@
       EngineSize : string
       NumberOfDoors : int }
 
-let car =
+let car1 =
     { Manufacturer = "Skoda"
       EngineSize = "small"
       NumberOfDoors = 4}
+
+let car2 =
+    { Manufacturer = "Skoda"
+      EngineSize = "small"
+      NumberOfDoors = 4}
+
+open System
+
+let x = car1.Equals(car2)
+let y = Object.ReferenceEquals(car1,car2)
+
+let r = Random()
+
+let newCar =
+    let rCar =
+        { car1 with
+            NumberOfDoors = r.Next(18,45)}
+
+    Console.WriteLine( "Old car {0}. New car {1}", car1.NumberOfDoors, rCar.NumberOfDoors)
+    rCar
+
+//let g() = r.Next 10
+//let h(min,max) = r.Next(min,max)
