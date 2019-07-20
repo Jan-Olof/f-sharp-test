8729ba31acc8f4ac26de54b6c844783b2a0482f4
Jan-Olof Nilsson <janolof@gmail.com>
2019-07-20 11:01:36 +0200

    11a

---
+++ f-sharp-test/scripts/11.fsx
@@ -0,0 +1,13 @@
+﻿open System
+open System.IO
+let writeToFile (date:DateTime) filename text =
+    let path = sprintf "D:\\tmp\\%s-%s.txt" (date.ToString "yyyyMMdd") filename
+    //let path = "D:\\tmp\\" + date.ToString("yyMMdd") + "-" + filename + ".txt"
+    File.WriteAllText(path, text)
+
+let writeToToday = writeToFile DateTime.Now.Date
+let writeToTomorrow = writeToFile (DateTime.Now.Date.AddDays 1.)
+
+let x = writeToToday "hello-world"
+
+x "some text"
