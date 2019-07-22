b49b98283a5e84d1ed2aefd0fbbcf8c3e8897a1b
Jan-Olof Nilsson <janolof@gmail.com>
2019-07-22 17:44:35 +0200

    11b

---
+++ f-sharp-test/scripts/11-pipeline.fsx
@@ -0,0 +1,11 @@
+﻿let drive distance petrol =
+    if distance = "far" then petrol / 2.0
+    elif distance = "medium" then petrol - 10.0
+    else petrol - 1.0
+
+let petrol = 100.0
+
+petrol
+|> drive "far"
+|> drive "medium"
+|> drive "short"
