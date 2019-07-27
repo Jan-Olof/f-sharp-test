2c5f5d1801a3ca63f763d7e1caf193221ff8e6a5
Jan-Olof Nilsson <janolof@gmail.com>
2019-07-27 14:29:53 +0200

    14b

--- Capstone2/Scrapbook.fsx
+++ Capstone2/Scrapbook.fsx
@@ -17,4 +17,34 @@ let acc1 = { Id = Guid.Parse("7c8e9d1c-1724-4511-8363-84a5576b0100"); Owner = do
 
 /// Deposits an amount into an account
 let deposit amount account : Account =
-    { account with Balance = amount }
+    { account with Balance = account.Balance + amount }
+
+let acc1a = deposit 100m acc1
+let acc1b = deposit 101m acc1a
+
+/// Withdraws an amount into out of an account
+let withdraw amount account : Account =
+    if amount > account.Balance then account
+    else { account with Balance = account.Balance - amount }
+
+let acc1c = acc1 |> withdraw 100m
+let acc1d = acc1b |> withdraw 100m
+
+let acc2 = acc1 |> deposit 100m |> withdraw 55m
+
+open System.IO
+
+///
+let fileSystemAudit account message =
+    let path = sprintf "d:\\tmp\\%s.txt" (account.Id.ToString())
+    let msg = sprintf "\n%s" message
+    File.AppendAllText(path, msg)
+
+fileSystemAudit acc1 "Buu"
+fileSystemAudit acc1 "Buu2"
+
+///
+let console account message =
+    printfn "Account %s: %s" (account.Id.ToString()) message
+
+console acc1 "Testing without meaning"
